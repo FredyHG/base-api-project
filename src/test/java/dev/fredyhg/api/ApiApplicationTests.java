a78fc70f28ced8273af633423464c4fc29f6c176
fredyhg <fredyultima2@gmail.com>
2023-04-05 23:06:58 -0300

    init project

---
+++ src/test/java/dev/fredyhg/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fredyhg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
